e39e2625ea97d81ff49841f6b352953fabb84677
Max Solovev <ms.solovev@jet.su>
2023-08-16 00:30:11 +0300

    Обновлен скрипт апдейта

--- update_and_rebuild.sh
+++ update_and_rebuild.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Пуллим последние изменения из репозитория
-git pull
-
-# Пересобираем Docker образ
-docker-compose build
-
 # Перезапускаем контейнеры
-docker-compose down
+docker-compose pull
 docker-compose up -d
